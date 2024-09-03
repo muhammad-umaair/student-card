ec75dcd49f8d62cc9af0e690fbd6fe3c7cd62fba
Muhammad Umair <mohammad.umair9610@gmail.com>
2024-09-03 20:08:55 +0500

    only popup closed when click cross button

--- scripts/script.js
+++ scripts/script.js
@@ -469,15 +469,27 @@ function showModal(extraRows) {
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
+    
+  document.getElementById('card-image').style.display = 'block';
+  document.getElementById('download-csv').style.display = 'none';
   };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target === modal) {
-      modal.style.display = "none";
+      event.stopPropagation();
+      // modal.style.display = "none";
     }
   };
 }
+
+
+
+document.querySelector('.close').addEventListener('click', function() {
+  // Close the modal (optional, if you're hiding the modal)
+  document.getElementById('cardLimitModal').style.display = 'none';
+  location.reload();
+});
 
 
 
